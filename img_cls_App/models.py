d749ef032de574db7131694b3789d4e425980ec8
Virendra <107255416+Virendra513@users.noreply.github.com>
2024-08-20 13:47:45 +0530

    Update models.py

--- img_cls_App/models.py
+++ img_cls_App/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-
-# Create your models here.
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class MyModel(models.Model):
     image = CloudinaryField('image')
 
-    def __str__(self):
-        return f"Image: {self.image.url}"
+    # def __str__(self):
+    #     return f"Image: {self.image.url}"
+
