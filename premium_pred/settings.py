95ccedca62804a84d113c2e5baa23baa56944de2
Virendra <107255416+Virendra513@users.noreply.github.com>
2024-08-20 12:06:52 +0530

    Update settings.py

--- premium_pred/settings.py
+++ premium_pred/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()
 SECRET_KEY = 'django-insecure-un4-^9#k6ino=2da3#eg5bi(4n#uu7cgon%yx3a(37(((izclm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
